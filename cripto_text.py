70f65138a4a3a4d0fa3b5d8cea34c435f8a3e513
Axl-Mont <montenegroaxel@outlook.com>
2023-10-29 01:03:50 -0300

    se creo un diccionario para manejejar las reglas del encriptado

--- cripto_text.py
+++ cripto_text.py
@@ -1,36 +1,31 @@
 import getpass
 
-text_input =(getpass.getpass('Ingresa una frase para encriptarla => ')).lower()
+rules = {
+    'A': '4',
+    'E': '3',
+    'I': '1',
+    'O': '0',
+    'U': 'ü',
+    'S': '5',
+    'T': '7',
+    'G': '6',
+    'B': '8',
+    ',': '^',
+    '.': '*',
+    '0': 'O',
+    '?': '#',
+    '1': 'L'
+}
 
 encript_text = ''
 
+text_input =(getpass.getpass('Ingresa una frase para encriptarla => ')).upper()
+
 for letter in text_input:
-    if letter == 'a':
-        encript_text += '4'
-    elif letter == 'e':
-        encript_text += '3'
-    elif letter == 'i':
-        encript_text += '1'
-    elif letter == 'o':
-        encript_text += '0'
-    elif letter == 'u':
-        encript_text += 'ü'
-    elif letter == ' ':
-        encript_text += '-'
-    elif letter == 's':
-        encript_text += '5'
-    elif letter == 't':
-        encript_text += '7'
-    elif letter == 'g':
-        encript_text += '6'
-    elif letter == 'b':
-        encript_text += '8'
-    elif letter == ',':
-        encript_text += '^'
-    elif letter == '.':
-        encript_text += '*'
+    if letter in rules:
+        encript_text += rules.get(letter)
     else:
         encript_text += letter
 
 print('Frace encriptada =>')
-print(encript_text.upper()[::-1])
+print(encript_text[::-1])
