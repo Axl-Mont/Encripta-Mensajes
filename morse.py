2c1a65b1a0e648a04bcf4419e624f87c9de11c41
Axl-Mont <montenegroaxel@outlook.com>
2023-11-06 19:17:16 -0300

    se corigio el metodo para buscar en el diccionario

--- morse.py
+++ morse.py
@@ -8,7 +8,7 @@
     para convertir entre texto y código Morse.
 """
 morse_rules = {
-    'A': '.-', 
+    'A': '.-',
     'B': '-...',
     'C': '-.-.',
     'D': '-..',
@@ -63,28 +63,31 @@ translator_rules = {value: key for key, value in morse_rules.items()}
 def convert_morse():
     """
         Convierte un mensaje de texto en código Morse.
-        
+
         El usuario ingresa un mensaje de texto. 
         Cada letra del mensaje se convierte en su equivalente en código Morse,
         Si un carácter no se puede convertir, se muestra tal cual.
     """
-    message_input = input('\nIngresa un mensaje para convertirlo a código Morse \n=> ').upper()
+    message_input = input(
+        '\nIngresa un mensaje para convertirlo a código Morse \n=> ').upper()
     morse_text = ''
 
     for character in message_input:
         if character in morse_rules:
-            morse_text += morse_rules[character] + ' '
+            morse_text += morse_rules.get(character) + ' '
         else:
-            print(f'El caracter {character} no se pudo convertir a Morse, se mostrará {character}')
+            print(
+                f'El caracter {character} no se pudo convertir a Morse, se mostrará {character}')
             morse_text += character + ' '
 
     print('Tu mensaje en Morse =>')
     print(morse_text)
 
+
 def morse_translator():
     """
         Traduce un mensaje en código Morse a texto.
-    
+
         El usuario ingresa un mensaje en código Morse. 
         Cada secuencia Morse se traduce a su equivalente en texto,
         Si una secuencia Morse no se puede traducir, se muestra tal cual fue ingresada.
@@ -96,27 +99,32 @@ def morse_translator():
 
     for morse in morse_characters:
         if morse in translator_rules:
-            message.append(translator_rules[morse])
+            message.append(translator_rules.get(morse))
         else:
-            print(f'El caracter {morse} no está en Morse, se imprimirá {morse}')
+            print(
+                f'El caracter {morse} no está en Morse, se imprimirá {morse}')
             message.append(morse)
 
     translated_message = ''.join(message)
     print('Mensaje traducido =>')
     print(translated_message)
 
+
 def main():
     """
         Función principal para ejecutar el traductor de Morse.
-        
+
         El usuario elige entre convertir un mensaje a Morse o traducir un mensaje en Morse a texto. 
         Se proporcionan instrucciones claras y se maneja la interacción con el usuario.
     """
     option = ('1', '2', '3')
+
     print('Bienvenido a Morse Code Translator, por favor elige una opción:')
 
     while True:
-        choice_user = input('\n1_ Convertir a Morse \n2_ Traducir de Morse \n3_ Salir\n==>')
+
+        choice_user = input(
+            '\n1_ Convertir a Morse \n2_ Traducir de Morse \n3_ Salir\n==>')
 
         if choice_user not in option:
             print('\nIngresa una opción correcta')
